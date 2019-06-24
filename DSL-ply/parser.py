a3516e373bf3c44b2db6295753d4e82ccc8c075f
v1eira <ewersonv@gmail.com>
2019-06-24 11:13:25 -0300

    change p -> entrada, t -> tokens

--- DSL-ply/parser.py
+++ DSL-ply/parser.py
@@ -32,7 +32,7 @@ tokens = [
     'DEF'
 ]
 
-# Definindo os tokens onde t = conjunto
+# Definindo os tokens onde tokens = conjunto
 
 t_PLUS = r'\+'
 t_MINUS = r'\-'
@@ -55,26 +55,26 @@ t_ignore = r' '
 
 
 # Definindo um numero 
-def t_NUMBER(t):
+def t_NUMBER(tokens):
     r'\d+\.?\d*([Ee][+-]?\d+)?'
-    t.value = float(t.value)
-    return t
+    tokens.value = float(tokens.value)
+    return tokens
 
-def t_DEF(t):
+def t_DEF(tokens):
     r'def'
-    t.type = 'DEF'
-    return t
+    tokens.type = 'DEF'
+    return tokens
 
 
 # Definindo name para variaveis, exige uma letra e pode ser seguido por letras ou numeros 
-def t_NAME(t):
+def t_NAME(tokens):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = 'NAME'
-    return t
+    tokens.type = 'NAME'
+    return tokens
 
-def t_error(t):
+def t_error(tokens):
     print("Não pode ser processado, verifique a entrada")
-    t.lexer.skip(1)
+    tokens.lexer.skip(1)
 
 lexer = lex.lex()
 
@@ -91,86 +91,86 @@ precedence = (
     ('left','LCHAVES','RCHAVES')
 )
 
-def p_contexto(p):
+def p_contexto(entrada):
     '''
     contexto    : deffuncao
                 | funcao
                 | expression
     '''
-    print(run(p[1]))
+    print(run(entrada[1]))
 
 
-def p_funcao(p):
+def p_funcao(entrada):
     '''
     funcao  : NAME values
     '''
-    contexto = p[1]
+    contexto = entrada[1]
     print("deu certo")
 
-def p_values(p):
+def p_values(entrada):
     '''
     values  : NAME
             | NUMBER
     '''
-    env['func'+contexto][2].add(p[1])
+    env['func'+contexto][2].add(entrada[1])
 
 
-def p_deffuncao(p):
+def p_deffuncao(entrada):
     '''
     deffuncao   : DEF NAME args bloco
     '''
-    p[0] = ('def', p[2],p[3],p[4])
+    entrada[0] = ('def', entrada[2],entrada[3],entrada[4])
 
 
-def p_args(p):
+def p_args(entrada):
     '''
     args    : LPAR NAME RPAR
     '''
-    p[0]= ('fc',p[2])
+    entrada[0]= ('fc',entrada[2])
 
-def p_args_largs(p):
+def p_args_largs(entrada):
     '''
     args    : largs
     '''
-    p[0] = p[1]
+    entrada[0] = entrada[1]
 
-def p_largs(p):
+def p_largs(entrada):
     '''
     largs   : args args
     '''
-    p[0] = ('fcs',p[1],p[2])
+    entrada[0] = ('fcs',entrada[1],entrada[2])
 
-def p_expression(p):
+def p_expression(entrada):
     '''
     expression  : term
                 | var_assign
                 | empty
     '''
-    p[0] = p[1]
+    entrada[0] = entrada[1]
     
 
-def p_bloco(p):
+def p_bloco(entrada):
     '''
     bloco   : LCHAVES expression RCHAVES
     '''
-    p[0] = p[2]
+    entrada[0] = entrada[2]
 
 
-#def p_linha(p):
+#def p_linha(entrada):
 #    '''
 #    linha   : expression ENDLINE
 #    '''
-#    p[0] = p[1]
+#    entrada[0] = entrada[1]
  
 # Definicao de como passar um valor para uma variavel 
-def p_var_assign(p):
+def p_var_assign(entrada):
     '''
     var_assign  : NAME EQUALS term
     '''
-    p[0] = ('=', p[1],p[3])
+    entrada[0] = ('=', entrada[1],entrada[3])
 
 # Definicao de expressao exige que a maior hierarquia apareca primeiro
-def p_term(p):
+def p_term(entrada):
     '''
     term    : term EXPONENT term
             | term MULTIPLY term
@@ -180,116 +180,116 @@ def p_term(p):
             | term MINUS term
             | term PLUS term
     '''
-    p[0] = (p[2], p[1], p[3])
+    entrada[0] = (entrada[2], entrada[1], entrada[3])
 
 
-def p_term_factor(p):
+def p_term_factor(entrada):
     '''
     term    : factor
     '''
-    p[0] = p[1]
+    entrada[0] = entrada[1]
 
 
-def p_factor(p):
+def p_factor(entrada):
     '''
     factor  : nterm
             | NUMBER
             | positive
             | negative
     '''
-    p[0] = p[1]
+    entrada[0] = entrada[1]
     
 
-def p_negative(p):
+def p_negative(entrada):
     '''
     negative    : MINUS term
     '''
-    p[0] = - p[2]
+    entrada[0] = - entrada[2]
 
-def p_positive(p):
+def p_positive(entrada):
     '''
     positive    : PLUS term
     '''
-    p[0] = p[2]
+    entrada[0] = entrada[2]
 
 
-def p_nterm(p):
+def p_nterm(entrada):
     '''
     nterm : LPAR term RPAR
     '''
-    p[0] = p[2]
+    entrada[0] = entrada[2]
 
 # Definicao de uma variavel 
-def p_term_var(p):
+def p_term_var(entrada):
     '''
     term    : NAME
     '''
-    p[0] = ('var', p[1])
+    entrada[0] = ('var', entrada[1])
 
-def p_error(p):
-    print("Syntax error found!" + p.value)
+def p_error(entrada):
+    print("Syntax error found!" + entrada.value)
 
-def p_empty(p):
+def p_empty(entrada):
     '''
     empty   :
     '''
-    p[0]= p
+    entrada[0]= entrada
 
 parser = yacc.yacc()
 # env é um dicionário que contém as variáveis. Este dicionário é global.
 
 
 # Executando a string de entrada
-def run(p):
+def run(entrada):
     global env
     global contexto
-    if type(p) == tuple:
-        if p[0] == '+':
-            return run(p[1]) + run(p[2])
-        elif p[0] == '-':
-            return run(p[1]) - run(p[2])
-        elif p[0] == '*':
-            return run(p[1]) * run(p[2])
-        elif p[0] == '/':
-            return run(p[1]) / run(p[2])
-        elif p[0] == '//':
-            return run(p[1]) // run(p[2])
-        elif p[0] == '%':
-            return run(p[1]) % run(p[2])
-        elif p[0] == '^':
-            return run(p[1]) ** run(p[2])
-        elif p[0] == '=':
+    if type(entrada) == tuple:
+        if entrada[0] == '+':
+            return run(entrada[1]) + run(entrada[2])
+        elif entrada[0] == '-':
+            return run(entrada[1]) - run(entrada[2])
+        elif entrada[0] == '*':
+            return run(entrada[1]) * run(entrada[2])
+        elif entrada[0] == '/':
+            return run(entrada[1]) / run(entrada[2])
+        elif entrada[0] == '//':
+            return run(entrada[1]) // run(entrada[2])
+        elif entrada[0] == '%':
+            return run(entrada[1]) % run(entrada[2])
+        elif entrada[0] == '^':
+            return run(entrada[1]) ** run(entrada[2])
+        elif entrada[0] == '=':
             if (contexto == ''):
-                env[p[1]] = run(p[2])
+                env[entrada[1]] = run(entrada[2])
             else:
-                env[contexto+p[1]] = run(p[2])
-        elif p[0] == 'var':
-            if p[1] in env:
-                return env[p[1]]
-            elif (contexto+p[1]) in env:
-                return env[contexto+p[1]]
+                env[contexto+entrada[1]] = run(entrada[2])
+        elif entrada[0] == 'var':
+            if entrada[1] in env:
+                return env[entrada[1]]
+            elif (contexto+entrada[1]) in env:
+                return env[contexto+entrada[1]]
             else:
                 return 'Undeclared variable found!'
-        elif p[0] == 'def':
-            print(p[1])
-            env['func'+p[1]] = []
-            env['func'+p[1]].append([])
-            env['func'+p[1]][0].append(p[3])
-            contexto = p[1]
-            run(p[2])
+        elif entrada[0] == 'def':
+            print(entrada[1])
+            env['func'+entrada[1]] = []
+            env['func'+entrada[1]].append([])
+            env['func'+entrada[1]][0].append(entrada[3])
+            contexto = entrada[1]
+            run(entrada[2])
             contexto=''
             print('declarando funcao')
-        elif p[0] == 'fcs':
-            run(p[1])
-            run(p[2])
-        elif p[0] == 'fc':
-            env['func'+contexto][0].append(p[1])
-        elif p[0] == 'func':
-            contexto = p[1]
-            #run(p[2])
+        elif entrada[0] == 'fcs':
+            run(entrada[1])
+            run(entrada[2])
+        elif entrada[0] == 'fc':
+            env['func'+contexto][0].append(entrada[1])
+        elif entrada[0] == 'func':
+            contexto = entrada[1]
+            #run(entrada[2])
             print('executando funcao')
     else:
-        return p
+        return entrada
 
 
 def rodaFuncao ():
